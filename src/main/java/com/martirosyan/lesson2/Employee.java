0e5cc2bb83cab8901c5e5f091ffaa38c3f9f5739
MTigra <tikol2008@gmail.com>
2020-06-22 11:12:17 +0300

    HW done

---
+++ src/main/java/com/martirosyan/lesson2/Employee.java
@@ -0,0 +1,54 @@
+package com.martirosyan.lesson2;
+
+public class Employee {
+    private String name;
+    private String email;
+    private int age;
+    private String position;
+
+    public void showInfo(){
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age='" + age + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
---
+++ src/main/java/com/martirosyan/lesson2/NoEnoughPlaceException.java
@@ -0,0 +1,6 @@
+package com.martirosyan.lesson2;
+
+public class NoEnoughPlaceException extends RuntimeException {
+
+
+}
