f94eb46f3d5078da0f664942062073fa99d4e419
MTigra <tikol2008@gmail.com>
2020-07-20 05:33:11 +0300

    hw13

---
+++ src/main/java/com/martirosyan/lesson13/MainApp.java
@@ -0,0 +1,88 @@
+package com.martirosyan.lesson13;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    // TASK 1
+    private int findCountOfStrings(String path, String str) {
+        int count = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            int ch;
+            int i = 0;
+            boolean flag = false;
+
+            while ((ch = reader.read()) != -1) {
+
+
+                if (i == 0 && flag) {
+                    count++;
+                }
+
+                if (((char) ch) == str.charAt(i)) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+                i = ++i % str.length();
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+    // TASK 2
+    private void joinAllFilesInDirectory(String directoryPath) {
+        File dir = new File(directoryPath);
+        File newfile = new File("newfile");
+        try {
+            newfile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Arrays.stream(dir.listFiles()).forEach(f -> fileConsumer(f, newfile));
+    }
+
+    private void fileConsumer(File f, File newfile) {
+        try (InputStream in = new BufferedInputStream((new FileInputStream(f)));
+             OutputStream out = new BufferedOutputStream(new FileOutputStream(newfile))) {
+
+            int ch;
+            while ((ch = in.read()) != -1) {
+                out.write(ch);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // TASK 3
+    private boolean removeRecursively(String directoryPath) {
+        File dir = new File(directoryPath);
+        File[] filelist = dir.listFiles();
+        if (filelist != null) {
+            for (File file : filelist) {
+                removeRecursively(file.getAbsolutePath());
+            }
+        }
+        return dir.delete();
+
+    }
+}
